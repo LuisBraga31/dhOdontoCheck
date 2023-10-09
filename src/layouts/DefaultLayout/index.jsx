7e23fba4f0fee2870908b9d9d94345f7fca254f8
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-10-08 22:16:19 -0300

    Ajustes na Home

--- src/layouts/DefaultLayout/index.jsx
+++ src/layouts/DefaultLayout/index.jsx
@@ -1,20 +1,12 @@
-import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import Navbar from '../../Components/NavBar/Navbar'
 import Footer from '../../Components/Footer/Footer'
 import { OdontoContext } from "../../contexts/globalContext";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 
 export function DefaultLayout() {
   
   const { tema } = useContext(OdontoContext);
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (location.pathname === '/') {
-      navigate("/home");
-    }
-  });
 
   return (
     <>
